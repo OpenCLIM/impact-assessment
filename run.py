9654047a1715806d3fbfb6f3e077312b7f2db05f
OliviaButters <111284147+OliviaButters@users.noreply.github.com>
2023-10-31 11:07:11 +0000

    Update run.py

--- run.py
+++ run.py
@@ -10,6 +10,11 @@ import geopandas as gpd
 import shutil
 import numpy as np
 from shapely.geometry import shape
+import rtree
+import re
+from shapely.geometry import shape, Point
+import datetime
+from operator import itemgetter
 
 # Set basic data paths
 data_path = os.getenv('DATA','/data')
@@ -27,6 +32,147 @@ uprn_lookup = glob(os.path.join(inputs_path, 'uprn', '*.csv'))
 parameters_path=os.path.join(inputs_path,'parameters')
 udm_para_in_path = os.path.join(inputs_path, 'udm_parameters')
 
+categorys_path = os.path.join(inputs_path, 'categories')
+if not os.path.exists(categorys_path):
+    os.mkdir(categorys_path)
+
+def process_data(ev):
+    
+    T_start = datetime.datetime.now()
+    builds_file = all_buildings
+    input_folder = run_path
+    
+    print(builds_file)
+        
+    #Creating the spatial index with the rtree module is only done for ine depth file using X,Y only
+    print('..creating spatial index..')
+    
+    #Find the csv file:
+    csv_lookup = glob(os.path.join(run_path, '*.csv'))
+     
+    #first get the resolution of the grid:
+    df_res = pd.read_csv(csv_lookup[0], nrows = 3)
+    xdiff = df_res.iloc[2, 0] - df_res.iloc[1, 0]
+    ydiff = df_res.iloc[2, 1] - df_res.iloc[1, 1]
+    if xdiff != 0:
+        dx = xdiff
+    elif xdiff == 0:
+        dx = ydiff
+    del(df_res)
+    buffer_distance = ((buffer_value)/100) * dx # in % grid resolution
+    
+    x = []
+    y = []
+    with open(csv_lookup[0], 'r') as t:
+        aline = t.readline().strip()
+        aline = t.readline()
+        while aline != '':
+            column = re.split('\s|\s\s|\t|,',str(aline))
+            x.append(float(column[0]))
+            y.append(float(column[1]))
+            aline = t.readline()
+    t.close()
+    
+    cell_idx = []
+    for idx, xi in enumerate(x):
+        cell_idx.append(idx)
+        
+    index = rtree.index.Index() #create the spatial index
+    for pt_idx, xi, yi in zip(cell_idx, x, y):
+        index.insert(pt_idx, (xi, yi))
+        
+    del(cell_idx)
+    
+    cell_index = []
+    buffer_list = []
+    builds = builds_file
+    builds_n = len(builds)
+    builds_df = gpd.GeoDataFrame(builds[[str(builds_field1), 'geometry']])
+    del(builds)
+    
+    for b_id, b_geom in zip(builds_df[str(builds_field1)], builds_df['geometry']):
+        buffer = shape(b_geom.buffer(float(buffer_distance), resolution=10)) #create a buffer polygon for the building polygons from resolution 10 to 16
+        for cell in list(index.intersection(buffer.bounds)): #first check if the point is within the bounding box of a building buffer
+            cell_int = Point(x[cell], y[cell])  
+            if cell_int.intersects(buffer): #then check if the point intersects with buffer polygon
+                buffer_list.append(b_id) #store the building ID
+                cell_index.append(cell) #store the line inedex of the intersecting points
+                
+    df_b = pd.DataFrame(list(zip(buffer_list, cell_index)), columns = [str(builds_field1), 'cell'])
+    df_b = df_b.sort_values(by = ['cell'])
+    print('spatial index created')
+    
+    #------------------------------------------------------------------------------reading depth files 
+        
+    files = csv_lookup
+    print('files:',files)
+    
+    for i, filename in enumerate(files):
+        f = open(filename)
+        print('processing file: ' + str(filename))
+        Z=[]
+        aline = f.readline().strip()       
+        aline = f.readline()
+        while aline != '':
+            column = re.split('\s|\s\s|\t|,',str(aline))
+            Z.append(float(column[2]))
+            aline = f.readline()
+        f.close()
+        
+        #--------------------------------------------------------------------------spatial intersection and classification
+        #the next line reads the depth values from the file according to cell index from above and stores the depth with the intersecting building ID
+        df = pd.DataFrame(list(zip(itemgetter(*cell_index)(Z),buffer_list)), columns=['depth',str(builds_field1)])
+        del(Z)
+        
+        #based on the building ID the mean and maximum depth are established and stored in a new data frame:
+        mean_depth = pd.DataFrame(df.groupby([str(builds_field1)])['depth'].mean().astype(float)).round(3).reset_index(level=0).rename(columns={'depth':'mean_depth'}) 
+        p90ile_depth = pd.DataFrame(df.groupby([str(builds_field1)])['depth'].quantile(0.90).astype(float)).round(3).reset_index(level=0).rename(columns={'depth':'p90ile_depth'})
+        damages_df = pd.merge(mean_depth, p90ile_depth)
+        del(mean_depth, p90ile_depth)
+        
+        #calculate the damages according to the water depth in buffer zone and the type of the building
+        damages_df['Class'] = 'A) Low'
+        damages_df['Class'][(damages_df['mean_depth'] >= 0) & (damages_df['mean_depth'] < 0.10) & (damages_df['p90ile_depth'] < 0.30)] = 'A) Low'
+        damages_df['Class'][(damages_df['mean_depth'] >= 0) & (damages_df['mean_depth'] < 0.10) & (damages_df['p90ile_depth'] >= 0.30)] = 'B) Medium'
+        damages_df['Class'][(damages_df['mean_depth'] >= 0.10) & (damages_df['mean_depth'] < 0.30) & (damages_df['p90ile_depth'] < 0.30)] = 'B) Medium' 
+        damages_df['Class'][(damages_df['mean_depth'] >= 0.10) & (damages_df['p90ile_depth'] >= 0.30)] = 'C) High'  
+        
+        #------------------------------------------------------------------------------merge results with a copy of the building layer and create output files
+        builds_data = builds_file
+        builds_df = gpd.GeoDataFrame(builds_data[[str(builds_field1), 'geometry']])
+        finalf = builds_df.merge(damages_df, on = str(builds_field1), how = 'left') #the merging of the building shapefile
+        
+        finalf['Area'] = (finalf.area).astype(int)#calculate the area for each building
+        finalf.to_file(os.path.join(categorys_path, 
+                        location + '-' + ssp + '-' + year + '-' + depth1 + '_exposure.shp'))
+        class_low = (finalf['Class'] == 'A) Low').sum()
+        class_medium = (finalf['Class'] == 'B) Medium').sum()        
+        class_high = (finalf['Class'] == 'C) High').sum()
+        del(damages_df)
+        
+        del(finalf['geometry'])
+        finalf_csv = pd.DataFrame(finalf)
+        finalf_csv.to_csv(os.path.join(categorys_path, 
+                            location + '-' + ssp + '-' + year + '-' + depth1 + '_exposure.csv'))
+        
+        del(builds_data, builds_df, finalf, finalf_csv, df)
+        
+        with open(os.path.join(outputs_path,
+                    location + '-' + ssp + '-' + year + '-' + depth1 + '_exposure_summary.txt'), 'w') as sum_results:
+            sum_results.write('Summary of Exposure Analysis for: ' + str(filename) + '\n\n'
+                        + 'Number of Buildings: ' + str(builds_n) + '\n'
+                        + 'Grid Resolution: ' + str(dx) + 'm' + '\n'
+                        + 'Buffer Distance: ' +str(buffer_value) + '% or' + str(buffer_distance) + 'm' + '\n\n'
+                        + 'Low: ' + str(class_low) + '\n'
+                        + 'Medium: ' +str(class_medium) + '\n'
+                        + 'High: ' +str(class_high) + '\n\n')
+            sum_results.close()
+            
+    del(x, y)
+    del(buffer_list, cell_index, df_b)
+    print('The Exposure Analysis is Finished. Time required: ' + str(datetime.datetime.now() - T_start)[:-4])
+
+
 # Identify the CityCat output raster
 archive = glob(run_path + "/*.tif", recursive = True)
 
@@ -41,11 +187,6 @@ buildings = glob(flood_impact_path + "/*.gpkg", recursive = True)
 # Search for a parameter file which outline the input parameters defined by the user
 parameter_file = glob(parameters_path + "/*.csv", recursive = True)
 print('parameter_file:', parameter_file)
-
-# Define output path for parameters
-parameters_out_path=os.path.join(outputs_path,'parameters')
-if not os.path.exists(parameters_out_path):
-    os.mkdir(parameters_out_path)
  
 # If the parameter file exists, read in the location, chosen SSP, year and storm event
 if len(parameter_file) == 1 :
@@ -64,11 +205,18 @@ if len(parameter_file) == 1 :
     year = parameters.loc[2][1]
     depth1 = parameters.loc[4][1]
     
+    # Define output path for parameters
+    parameters_out_path=os.path.join(outputs_path,'parameters')
+    if not os.path.exists(parameters_out_path):
+        os.mkdir(parameters_out_path)
+    
 if len(parameter_file) == 0:
     location = os.getenv('LOCATION')
     ssp = os.getenv('SSP')
     year = os.getenv('YEAR')
     depth1 = os.getenv('TOTAL_DEPTH')
+    
+dtmres = int(os.getenv('DTM_RESOLUTION'))
 
 # Read in the baseline builings
 with rio.open(archive[0],'r+') as max_depth :
@@ -105,9 +253,40 @@ with rio.open(archive[0],'r+') as max_depth :
     else :
         # If there are no udm buildings, all the buildings in the simulation are defined by the baseline buildings
         all_buildings = e_builds
+        
+    # Create a list of all of the column headers in the buildings file:
+    cols_list = []
+    for n in all_buildings.columns:
+        cols_list.append(n)
+    print('cols_list:',cols_list)
 
-    # Create a copy of the original geometry
-    all_buildings['geometry_copy'] = all_buildings['geometry']
+    #import the field of buildings
+    builds_field1 = "toid" #check the field from the shapefile
+    builds_field2 = "building_use" #check the field from the shapefile
+    
+    if dtmres == 5:
+        buffer_value = 100
+    else:
+        buffer_value = 150
+    print('buffer_value:',buffer_value)
+    
+    process_data('Risk_Levels')
+    
+    #Create a copy of the original geometry
+    #all_buildings['geometry_copy'] = all_buildings['geometry']
+    
+    class_data = glob(categorys_path + "/*.shp", recursive = True)
+    class_data = gpd.read_file(class_data[0], bbox=max_depth.bounds)
+    
+    all_buildings.set_crs(epsg=27700, inplace=True)
+    class_data.set_crs(epsg=27700, inplace=True)
+
+    all_buildings1 = gpd.sjoin(all_buildings, class_data,how='left',op="contains")
+    
+    #all_buildings1.to_file(os.path.join(outputs_path, 'builds.gpkg'),driver='GPKG')
+    
+    #Create a copy of the original geometry
+    all_buildings1['geometry_copy'] = all_buildings1['geometry']
     
     # Read flood depths and vd_product
     depth = max_depth.read(1)
@@ -119,71 +298,68 @@ with rio.open(archive[0],'r+') as max_depth :
             transform=max_depth.transform)], crs=max_depth.crs)
 
     # Store original areas for damage calculation
-    all_buildings['original_area'] = all_buildings.area
+    all_buildings1['original_area'] = all_buildings1.area
 
     # Buffer buildings
-    all_buildings['geometry'] = all_buildings.buffer(buffer)
+    all_buildings1['geometry'] = all_buildings1.buffer(buffer)
 
     # Extract maximum depth and vd_product for each building
-    all_buildings['depth'] = [row['max'] for row in
-                        zs(all_buildings, depth, affine=max_depth.transform, stats=['max'],
+    all_buildings1['depth'] = [row['max'] for row in
+                        zs(all_buildings1, depth, affine=max_depth.transform, stats=['max'],
                             all_touched=True, nodata=max_depth.nodata)]
 
     # Filter buildings
-    all_buildings = all_buildings[all_buildings['depth'] > threshold]
+    all_buildings1 = all_buildings1[all_buildings1['depth'] > threshold]
+
+    # Filter buildings
+    all_buildings1 = all_buildings1[all_buildings1['Class'] != "A) Low"]
 
     # Calculate depth above floor level
-    all_buildings['depth'] = all_buildings.depth - threshold
+    all_buildings1['depth'] = all_buildings1.depth - threshold
     
     # If no buildings are flooded, write an empty excel sheet and exit the code
-    if len(all_buildings) == 0:
+    if len(all_buildings1) == 0:
         with open(os.path.join(outputs_path, 'buildings.csv'), 'w') as f:
             f.write('')
         exit(0)                   
+                
+    # all_buildings1.to_csv(
+    #       os.path.join(outputs_path, 'affected_buildings_' + location + '_' + ssp + '_'  + year + '_' + depth1 +'mm.csv'), index=False,  float_format='%g')                  
                                 
     # Read in the preassigned damage curves
     residential = pd.read_csv(os.path.join(dd_curves_path, 'residential.csv'))
     nonresidential = pd.read_csv(os.path.join(dd_curves_path, 'nonresidential.csv'))
 
     # Calculate damage based on property types
-    res_data = all_buildings.loc[all_buildings['building_u']=='residential']
-    non_res_data = all_buildings.loc[all_buildings['building_u']!='residential']
+    res_data = all_buildings1.loc[all_buildings1['building_u']=='residential']
+    non_res_data = all_buildings1.loc[all_buildings1['building_u']!='residential']
 
     res_data['damage'] = (np.interp(
         res_data.depth, residential.depth, residential.damage) * res_data.original_area).round(0)
     non_res_data['damage'] = (np.interp(
         non_res_data.depth, nonresidential.depth, nonresidential.damage) * non_res_data.original_area).round(0).astype(int)
 
-    all_buildings=res_data.append(non_res_data)
+    all_buildings1=res_data.append(non_res_data)
 
-    # # Get the flooded perimeter length for each building
-    # flooded_perimeter = gpd.overlay(gpd.GeoDataFrame({'toid': all_buildings.toid}, geometry=all_buildings.geometry.boundary,
-    #                                                 crs=all_buildings.crs), flooded_areas)
-    # flooded_perimeter['flooded_perimeter'] = flooded_perimeter.geometry.length.round(2)
 
-    # all_buildings['perimeter'] = all_buildings.geometry.length
-
-    # all_buildings = all_buildings.merge(flooded_perimeter, on='toid', how='left')
-    # all_buildings['flooded_perimeter'] = all_buildings.flooded_perimeter.divide(
-    #     all_buildings.perimeter).fillna(0).multiply(100).round(0).astype(int)
-
-    # Lookup UPRN if available
-    if len(uprn_lookup) > 0:
-        uprn = pd.read_csv(uprn_lookup[0], usecols=['IDENTIFIER_1', 'IDENTIFIER_2'],
-                        dtype={'IDENTIFIER_1': str}).rename(columns={'IDENTIFIER_1': 'uprn',
-                                                                        'IDENTIFIER_2': 'toid'})
-        all_buildings = all_buildings.merge(uprn, how='left')
+    # # Lookup UPRN if available
+    # if len(uprn_lookup) > 0:
+    #     uprn = pd.read_csv(uprn_lookup[0], usecols=['IDENTIFIER_1', 'IDENTIFIER_2'],
+    #                     dtype={'IDENTIFIER_1': str}).rename(columns={'IDENTIFIER_1': 'uprn',
+    #                                                                     'IDENTIFIER_2': 'toid'})
+    #     if len(uprn) != 0:
+    #         all_buildings1 = all_buildings1.merge(uprn, how='left')
 
     # Create a new data frame called centres which is a copy of buildings
-    building_centroid=all_buildings.filter(['building_u','geometry_copy','damage','depth'])
+    building_centroid=all_buildings1.filter(['building_u','geometry_copy','damage','depth','Class'])
     building_centroid['geometry'] = building_centroid['geometry_copy']
     building_centroid.pop('geometry_copy')
     building_centroid.crs=e_builds.crs
-    all_buildings.pop('geometry_copy')
+    all_buildings1.pop('geometry_copy')
 
     building_centroid['building_u']=building_centroid['building_u'].fillna('unknown')
     
-    # Save building centroids and their geometrys to CSV
+    #Save building centroids and their geometrys to CSV
     # building_centroid.to_csv(
     #     os.path.join(outputs_path, 'affected_buildings_' + location + '_' + ssp + '_'  + year + '_' + depth1 +'mm.csv'), index=False,  float_format='%g') 
     
@@ -195,7 +371,7 @@ with rio.open(archive[0],'r+') as max_depth :
 # Create a geo dataframe for the centroids
 centre = gpd.GeoDataFrame(building_centroid,geometry="geometry",crs="EPSG:27700")
 
-# Apply the centroid function to the geometry column to determin the centre of each polygon
+# Apply the centroid function to the geometry column to determine the centre of each polygon
 centre.geometry=centre['geometry'].centroid
 
 grid.set_crs(epsg=27700, inplace=True)
@@ -203,6 +379,9 @@ grid.set_crs(epsg=27700, inplace=True)
 pointsInPolygon = gpd.sjoin(grid,centre, how="left", op="intersects")
 
 pointsInPolygon=pointsInPolygon.fillna(0)
+
+# pointsInPolygon.to_csv(
+#             os.path.join(outputs_path, 'pointsInPolygon_' + location + '_' + ssp + '_'  + year + '_' + depth1 +'.csv'), index=False,  float_format='%g') 
 
 dfpivot = pd.pivot_table(pointsInPolygon,index='tile_name',
                         columns='building_u',aggfunc={'building_u':len}, fill_value=0)
@@ -220,6 +399,8 @@ half_data = pd.merge(stacked,grid, on='tile_name')
 all_data = pd.merge(dfpivot2,half_data, on='tile_name')
 
 check = list(all_data.columns.values)
+
+print('check:',check)
 
 #all_data['Total_Building_Count'] = all_data['index_right']
 all_data.pop('index_right')
